907d718e05ed84b42211f63b5e6bd0653820b5d0
Monabbir-Ahmmad <57152487+Monabbir-Ahmmad@users.noreply.github.com>
2020-12-30 00:39:09 +0600

    Updated pie chart

--- Hishab/app/src/main/java/com/example/hishab/StatisticsFragment.java
+++ Hishab/app/src/main/java/com/example/hishab/StatisticsFragment.java
@@ -25,6 +25,7 @@ import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.MPPointF;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StatisticsFragment extends Fragment {
 
@@ -61,84 +62,105 @@ public class StatisticsFragment extends Fragment {
     //This creates the pie chart
     private void createPieChart() {
 
-        //This gets a color according to theme
-        TypedValue typedValue = new TypedValue();
-        getContext().getTheme().resolveAttribute(R.attr.colorBlackWhite, typedValue, true);
-
-        ArrayList<PieEntry> values = new ArrayList<>();
-
         String[] category = getResources().getStringArray(R.array.Category);
+
+        int[] colorArray = getContext().getResources().getIntArray(R.array.colorArray);
+        List<Integer> colorList = new ArrayList<Integer>(colorArray.length);
+        for (int i : colorArray)
+            colorList.add(i);
+
+        //This gets a color according to theme
+        TypedValue color = new TypedValue();
+        getContext().getTheme().resolveAttribute(R.attr.colorBlackWhite, color, true);
+
         //This adds the values into the PieEntry
+        ArrayList<PieEntry> values = new ArrayList<>();
         for (int i = 0; i < category.length; i++)
-            values.add(new PieEntry((i * 2 + 3) * 7, category[i]));
+            values.add(new PieEntry(i + 1, category[i]));
 
 
         //This inserts the PieEntry into the PieDataSet
-        PieDataSet dataSet = new PieDataSet(values, "Category");
-        dataSet.setSliceSpace(2f);
+        PieDataSet dataSet = new PieDataSet(values, null);
+        dataSet.setSliceSpace(0f);
         dataSet.setSelectionShift(5f);
         dataSet.setIconsOffset(new MPPointF(0, 40));
-        dataSet.setColors(getContext().getResources().getIntArray(R.array.colorArray));
+        dataSet.setColors(colorArray);
 
-        // Outside values
+        //Outside values with line
         dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-        //dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-        dataSet.setValueLineColor(typedValue.data);
-        dataSet.setValueLinePart1OffsetPercentage(100f); /** When valuePosition is OutsideSlice, indicates offset as percentage out of the slice size */
-        dataSet.setValueLinePart1Length(0.5f); /** When valuePosition is OutsideSlice, indicates length of first half of the line */
-        dataSet.setValueLinePart2Length(0.5f); /** When valuePosition is OutsideSlice, indicates length of second half of the line */
+        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setValueLinePart1OffsetPercentage(100f);
+        dataSet.setValueLinePart1Length(0.2f);
+        dataSet.setValueLinePart2Length(0.15f);
+        dataSet.setValueLineWidth(2f);
+        dataSet.setUsingSliceColorAsValueLineColor(true);
 
 
         //This creates the PieData from PieDataSet
-        PieData data = new PieData(dataSet);
-        data.setValueTextSize(10f);
-        data.setValueTextColor(Color.WHITE);
-        pieChart.setEntryLabelColor(typedValue.data);
+        PieData pieData = new PieData(dataSet);
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColors(colorList);
+        pieData.setValueFormatter(new PercentFormatter(pieChart));
+
+        //Insert data
+        pieChart.setNoDataText("No data available");
+        pieChart.setData(pieData);
+
+        //Slice shape
+        pieChart.setDrawRoundedSlices(false);
+        pieChart.setDrawSlicesUnderHole(false);
+
+        //Entry label
+        pieChart.setDrawEntryLabels(false);
+        pieChart.setEntryLabelColor(color.data);
         pieChart.setEntryLabelTextSize(10f);
         pieChart.setUsePercentValues(true);
-        data.setValueFormatter(new PercentFormatter(pieChart));
 
-
-        //Legends are the detailed name of each slice
-        Legend legend = pieChart.getLegend();
-        legend.setEnabled(false);
-        legend.setTextColor(typedValue.data);
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setWordWrapEnabled(true);
-        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
-        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        legend.setDrawInside(true);
-
-        //Description of the chart
-        Description description = pieChart.getDescription();
-        description.setEnabled(false);
-        description.setTextColor(typedValue.data);
-
-        //Transparent Circle
+        //Transparent circle
         pieChart.setTransparentCircleRadius(50f);
         pieChart.setTransparentCircleColor(Color.BLACK);
 
         //Center hole
         pieChart.setDrawHoleEnabled(true);
-        pieChart.setHoleRadius(60f);
+        pieChart.setHoleRadius(85f);
         pieChart.setCenterTextSize(50f);
         pieChart.setCenterTextRadiusPercent(60f);
         pieChart.setHoleColor(Color.TRANSPARENT);
 
         //Center Text
-        pieChart.setCenterText("Monthly Expense");
-        pieChart.setCenterTextColor(typedValue.data);
+        pieChart.setCenterText("Weekly\nExpense");
+        pieChart.setCenterTextColor(color.data);
         pieChart.setCenterTextSize(20f);
 
         //Animation
         pieChart.animateXY(1000, 1000);
-        pieChart.setDragDecelerationFrictionCoef(0.99f);
+        pieChart.setDragDecelerationFrictionCoef(0.97f);
 
-        //Insert data
-        pieChart.setNoDataText("No data available");
-        pieChart.setExtraOffsets(0, 20, 0, 20);
-        pieChart.setData(data);
+        //Off set
+        pieChart.setExtraOffsets(25f, 0f, 25f, 0f);
+
+
+        //Legends are the detailed name of each slice
+        Legend legend = pieChart.getLegend();
+        legend.setEnabled(true);
+        legend.setTextColor(color.data);
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setFormSize(10f);
+        legend.setWordWrapEnabled(true);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        //legend.setXOffset(0f);
+        //legend.setYOffset(5f);
+        //legend.setXEntrySpace(10f);
+        legend.setYEntrySpace(7f);
+        legend.setDrawInside(false);
+
+        //Description of the chart
+        Description description = pieChart.getDescription();
+        description.setEnabled(false);
+        description.setTextColor(color.data);
+
     }
 
 
