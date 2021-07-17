df16e69d084204adc7b8491ff35c76ca82514632
Monabbir-Ahmmad <57152487+Monabbir-Ahmmad@users.noreply.github.com>
2021-07-17 22:58:19 +0600

    gradle fix

--- Hishab/app/src/main/java/com/example/hishab/ui/statistics/BarChartMarker.java
+++ Hishab/app/src/main/java/com/example/hishab/ui/statistics/BarChartMarker.java
@@ -19,7 +19,6 @@ public class BarChartMarker extends MarkerView {
     private final TextView textView;
     private final DecimalFormat decimalFormat = new DecimalFormat("#,###.##");
     private final String currency;
-
     //Constructor
     public BarChartMarker(Context context) {
         super(context, R.layout.marker_view);
